5daf0f38232136fd4599a61e6ac3901d4be67f0d
OLIX <80951658+OLIX-boop@users.noreply.github.com>
2024-04-08 22:11:58 +0200

    awd

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -12,6 +12,14 @@ export default function Register() {
     const [firstName, setFirstName] = useState<string>('');
     const [lastName, setLastName] = useState<string>('');
     const [error, setError] = useState('');
+    
+    
+    const [pswMatch, setPswMatch] = useState(true);
+    const [pswLength, setPswLength] = useState(true);
+
+    const handlePswLength = (e:React.FormEvent<HTMLInputElement>) => {
+        setPswLength((e.target.value.length >= 8));
+    }
 
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
@@ -36,7 +44,6 @@ export default function Register() {
         } catch (err) {
             setEmail('error');
         }
-
     }
     return (<>
         <form onSubmit={handleSubmit} className="flex flex-col gap-4 w-[30%] mt-[10vh] m-auto border-2 border-black justify-center align-middle py-9">
@@ -47,23 +54,23 @@ export default function Register() {
             <div className="grid grid-cols-2 w-[70%] mx-auto ">
                 <div>
                     <label className="mb-[-1vh]" htmlFor="name">First Name</label>
-                    <input className="border-2 mx-auto py-1 w-[90%] text-md hover:border-black duration-200" type="text" value={firstName} onChange={e => setFirstName(e.target.value || '')} id="name" />
+                    <input required className="border-2 mx-auto py-1 w-[90%] text-md hover:border-black duration-200" type="text" value={firstName} onChange={e => setFirstName(e.target.value || '')} id="name" />
                 </div>
 
                 <div>
                     <label className="mb-[-1vh]" htmlFor="name">Last Name</label>
-                    <input className="border-2 mx-auto w-[100%] py-1 text-md hover:border-black duration-200" type="text" value={lastName} onChange={e => setLastName(e.target.value || '')} id="lastname" />
+                    <input required className="border-2 mx-auto w-[100%] py-1 text-md hover:border-black duration-200" type="text" value={lastName} onChange={e => setLastName(e.target.value || '')} id="lastname" />
                 </div>
             </div>
 
             <label className="ml-[15%] mb-[-1vh]" htmlFor="email">Email</label>
-            <input className="border-2 w-[70%] mx-auto py-1 text-md hover:border-black duration-200" type="email" value={email} onChange={e => setEmail(e.target.value || '')}  id="email" />
+            <input required className="border-2 w-[70%] mx-auto py-1 text-md hover:border-black duration-200" type="email" value={email} onChange={e => setEmail(e.target.value || '')}  id="email" />
 
             <label className="ml-[15%] mb-[-1vh]" htmlFor="password">Password</label>
-            <input className="border-2 w-[70%] mx-auto py-1 text-md hover:border-black duration-200" id="password" value={password} onChange={e => setPassword(e.target.value || '')}  type="password" />
+            <input required onInput={e => handlePswLength(e)} className={"border-2 w-[70%] mx-auto py-1 text-md hover:border-black duration-200 "+(pswLength ? "" : "border-red-500")} id="password" value={password} onChange={e => setPassword(e.target.value || '')}  type="password" />
 
             <label className="ml-[15%] mb-[-1vh]" htmlFor="password">Confirm Password</label>
-            <input className="border-2 w-[70%] mx-auto py-1 text-md hover:border-black duration-200" id="password" value={passwordConfirm} onChange={e => setPasswordConfirm(e.target.value || '')}  type="password" />
+            <input required className={"border-2 w-[70%] mx-auto py-1 text-md hover:border-black duration-200 " + (pswMatch ? "" : "border-red-500 focus:border-red-500")} id="password" value={passwordConfirm} onChange={e => {setPasswordConfirm(e.target.value || ''); setPswMatch((password===passwordConfirm))}}  type="password" />
 
             <button type="submit" className="mx-auto bg-black text-white py-2 w-[70%] border-2 border-black font-bold hover:text-black hover:bg-white duration-150">Register</button>
 
