7a868aa16adab8e3f18fb6b52c25d200be30336f
OLIX <andreaolimpio@yahoo.com>
2024-04-09 18:16:23 +0200

    .

--- app/auth/register/page.tsx
+++ app/auth/register/page.tsx
@@ -16,12 +16,7 @@ export default function Register() {
     
     const [pswMatch, setPswMatch] = useState(true);
     const [pswLength, setPswLength] = useState(true);
-
-    const handlePswLength = (e:React.FormEvent<HTMLInputElement>) => {
-        setPswLength((e.target.value.length >= 8));
-    }
-
-
+    
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if(password !== passwordConfirm) return setError("Passwords doesn't match!");
@@ -45,6 +40,17 @@ export default function Register() {
             setEmail('error');
         }
     }
+
+    const handlePswMatch = (value:string) => {
+        setPasswordConfirm(value || '');
+        setPswMatch(password===value || passwordConfirm.length === 0 || password.length === 0);
+    }
+    const handlePswLength = (e:React.FormEvent<HTMLInputElement>) => {
+        setPswLength((e.target.value.length >= 8 || e.target.value.length === 0));
+        setPswMatch(passwordConfirm===e.target.value || passwordConfirm.length === 0 || password.length === 0);
+    }
+
+
     return (<>
         <form onSubmit={handleSubmit} className="flex flex-col gap-4 w-[30%] mt-[10vh] m-auto border-2 border-black justify-center align-middle py-9">
             <h1 className="text-2xl font-bold mx-auto">Register</h1>
@@ -67,10 +73,12 @@ export default function Register() {
             <input required className="border-2 w-[70%] mx-auto py-1 text-md hover:border-black duration-200" type="email" value={email} onChange={e => setEmail(e.target.value || '')}  id="email" />
 
             <label className="ml-[15%] mb-[-1vh]" htmlFor="password">Password</label>
-            <input required onInput={e => handlePswLength(e)} className={"border-2 w-[70%] mx-auto py-1 text-md hover:border-black duration-200 "+(pswLength ? "" : "border-red-500")} id="password" value={password} onChange={e => setPassword(e.target.value || '')}  type="password" />
+            <input required minLength={8} onInput={e => handlePswLength(e)} className={"border-2 w-[70%] mx-auto py-1 text-md hover:border-black duration-200 "+(pswLength ? "" : "border-red-500 focus:outline-red-500")} id="password" value={password} onChange={e => setPassword(e.target.value || '')}  type="password" />
+            <p className="text-red-500 ml-[15%] text-sm">{!pswLength && "Password requires 8 characters minimum"}</p>
 
             <label className="ml-[15%] mb-[-1vh]" htmlFor="password">Confirm Password</label>
-            <input required className={"border-2 w-[70%] mx-auto py-1 text-md hover:border-black duration-200 " + (pswMatch ? "" : "border-red-500 focus:border-red-500")} id="password" value={passwordConfirm} onChange={e => {setPasswordConfirm(e.target.value || ''); setPswMatch((password===passwordConfirm))}}  type="password" />
+            <input required className={"border-2 w-[70%] mx-auto py-1 text-md hover:border-black duration-200 " + (pswMatch ? "" : "border-red-500 focus:outline-red-500")} id="password" value={passwordConfirm} onChange={e => handlePswMatch(e.target.value)}  type="password" />
+            <p className="text-red-500 ml-[15%] text-sm">{(passwordConfirm.length > 0 && password.length > 0 && !pswMatch) ? "Passwords doesn't match" : ""}</p>
 
             <button type="submit" className="mx-auto bg-black text-white py-2 w-[70%] border-2 border-black font-bold hover:text-black hover:bg-white duration-150">Register</button>
 
