a0d314cfc1ddb95a2effb55a4836a465b87901a8
OLIX <andreaolimpio@yahoo.com>
2025-01-08 17:46:22 +0100

    added temp fix for certificate error

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0'; // temporary fix for: could not load certificate
 const nextConfig = {
     env: {
         NEXT_DB_IP: process.env.NEXT_PRODUCTION == "false" ?  "127.0.0.1": process.env.NEXT_DB_IP,
